7416d19e19b32f10375ba9606def38f23afa0c0f
Andrew Wentzel <tehwentzel@gmail.com>
2021-01-25 20:58:14 -0600

    made build windowed and also fixed double jump and you can now hold to charge pizza gun

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -9,7 +9,9 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 25.0f;
     public float rotationSpeed = 90;
-    public float force = 1f;
+    public float force = 2f;
+    public float chargeTimer;
+    public float groundHeight;
 
     public GameObject cannon;
     public GameObject bullet;
@@ -22,6 +24,12 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         t = GetComponent<Transform>();
+        chargeTimer = 0;
+        groundHeight = gameObject.transform.position.y;
+    }
+
+    void OnEnable(){
+        groundHeight = gameObject.transform.position.y;
     }
 
     // Update is called once per frame
@@ -40,14 +48,28 @@ public class PlayerMovement : MonoBehaviour
         else if (Input.GetKey(KeyCode.A))
             t.rotation *= Quaternion.Euler(0, -rotationSpeed * Time.deltaTime, 0);
 
-        if (Input.GetKeyDown(KeyCode.Space))
-            rb.AddForce(t.up * force);
+        if (Input.GetKeyDown(KeyCode.Space)){
+            if(gameObject.transform.position.y <= groundHeight*1.1){
+                rb.AddForce(t.up * force);
+            }
+        }
 
         // https://docs.unity3d.com/ScriptReference/Input.html
         if (Input.GetButtonDown("Fire1"))
         {
+            chargeTimer = Time.time;
+        }
+        else if (Input.GetButton("Fire1")){
+            if(Time.time - chargeTimer > 5){
+                Debug.Log("charged");
+            }
+        }
+        else if(Input.GetButtonUp("Fire1")){
+            float chargeMultiplier = Mathf.Max(1, 10*(Time.time - chargeTimer));
+            
             GameObject newBullet = GameObject.Instantiate(bullet, cannon.transform.position, cannon.transform.rotation) as GameObject;
             Vector3 bulletVelocity = (t.forward * 200) + t.up*10;
+            bulletVelocity = bulletVelocity * Mathf.Min(chargeMultiplier, 50.0f);
             newBullet.GetComponent<Rigidbody>().AddForce(bulletVelocity);
         }
 
